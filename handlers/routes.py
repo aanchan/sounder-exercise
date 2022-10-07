c261342fb8e3e62d1cf50859af24af1a4322c317
Aanchan Mohan <aanchan@synaptitude.info>
2022-10-07 14:07:14 -0700

    Added static input data to the analyze route, and input data into fake_db

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,5 +1,7 @@
-from flask import request, jsonify
+from flask import request
 import uuid
+
+fake_db = {}
 
 
 def configure_routes(app):
@@ -11,6 +13,9 @@ def configure_routes(app):
     @app.post('/analyze')
     def analyze_endpoint():
         key = uuid.uuid4()
+        data = request.get_json()
+        fake_db[key] = data
         print(f'Key:{key}')
+        print(f'fake_db_entry:{fake_db[key]}')
         return {"id": key}
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from handlers.routes import configure_routes
 import pytest
-
+import json
 
 @pytest.fixture
 def test_app():
@@ -22,8 +22,16 @@ def test_base_route(test_client):
     assert response.status_code == 200
 
 
-def test_analyze_route(test_client):
+@pytest.fixture
+def input_data():
+    file_name='input_text_data.json'
+    with open(file_name) as f:
+        data = json.load(f)
+        yield data
+
+
+def test_analyze_route(test_client, input_data):
     url = '/analyze'
-    response = test_client.post(url)
+    response = test_client.post(url, json=input_data)
     assert isinstance(response.get_json()["id"], str)
 
